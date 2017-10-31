63997412819e97fbe12a18aa4fb8d3493fee69c9
Ramdhas Ueikattan <ramdhas.ueikattan@syncfusion.com>
2017-10-31 14:09:26 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ItemModel } from '@syncfusion/ej2-navigations';
+import { gridData1 } from './data';
+import {ToolbarItems} from '@syncfusion/ej2-ng-grids';
+
+@Component({
+    selector: 'my-app',
+    template: ` <form><ej-grid #grid [dataSource]='data' [allowFiltering]='true' height='350px' [editSettings]='editSetting' 
+    (dataBound)='dataBoundHandler()' [toolbar]='toolbarItems' (toolbarClick)='tClick($event)'>
+                <e-columns>
+                    <e-column field='OrderID' isPrimaryKey='true' headerText='Order ID' textAlign='right' width=120></e-column>
+                    <e-column field='CustomerID' headerText='Customer ID' width=150 [validationRules]='validationRules'></e-column>
+                    <e-column field='ShipCity' headerText='Ship City' width=150></e-column>
+                    <e-column field='ShipName' headerText='Ship Name' width=150></e-column>
+                </e-columns>
+                </ej-grid> </form>
+
+                <ng-template #template>
+                  <span>DataCount - {{dataCount}}</span>
+                </ng-template>
+                <ng-template #template1>
+                <button type='button'>Print</button>
+              </ng-template>`
+})
+export class AppComponent implements OnInit {
+
+    public data: Object[];
+
+    @ViewChild('grid')
+    public grid;
+    @ViewChild('template')
+    public rowCount;
+    @ViewChild('template1')
+    public refresh;
+    public editSetting: Object;
+    public validationRules;
+    public toolbarItems: (ToolbarItems | ItemModel)[];
+
+    public dataCount;
+
+    ngOnInit(): void {
+        this.data = gridData1;
+        this.toolbarItems = ['add', 'delete', 'cancel', 'update', 'edit', {template: this.rowCount}, {template: this.refresh}];
+        this.validationRules= {required:true};
+        this.editSetting= {allowEditing: true, allowDeleting: true, allowAdding: true};
+    }
+    tClick(){
+        this.grid.print();
+    }
+    dataBoundHandler() {
+      // update data count
+      this.dataCount = this.grid.currentViewData.length;
+    }
+}
